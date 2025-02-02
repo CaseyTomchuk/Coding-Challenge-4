3de48cb6b46ac7f53bf89f7ef72cfc4c359d3e11
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-02-02 18:53:27 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+// Task 1: If Statements
+let purchaseAmount = 160;
+
+if (purchaseAmount > 150) {
+    discountPurchase = purchaseAmount / 2
+    console.log(`Final amount after discount: $${discountPurchase}`);
+} // if this purchase amount is > 150 then I will create a new variable that is the half the purchase amount and log it to the console
+else {
+    console.log(`Final amount: $${purchaseAmount}`);
+} //if there is no discount, I will just log the original purchase amount
