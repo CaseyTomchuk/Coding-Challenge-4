41faa6df3f495341fcb2c13e7659a10aa8f7b5bf
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-02-02 19:22:42 -0500

    Task 3 - Inventory Depletion

--- cc_4.js
+++ cc_4.js
@@ -17,3 +17,10 @@ for (let i = 0; i < sales.length; i++) {
     sum += sales[i];
 }  //let i = 0 sets the index to start at 0. As long as i is less than the amount of items left in the array, the loop will repeat, running sum+=sales[i]
 console.log(`The reported sales figures are: ${sales} and the total sales adds up to: $${sum}`); //note: wasn't sure how to make each item in the sales array appear with a $
+
+// Task 3: While Loop
+let stock = 10;
+while (stock >= 0) {
+    updatedStock = stock-- //As long as the stock is greater than or equal to 0, the stock will be subtracted by 1
+    console.log(`Updated stock: ${updatedStock}`);
+};
